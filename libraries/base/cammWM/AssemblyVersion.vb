6b88c1201893946dbae57e09fa7cb9b5e57d7e31
Jochen Wezel <jwezel@compumaster.de>
2015-11-23 17:11:56 +0100

    updated assembly version

--- libraries/base/cammWM/AssemblyVersion.vb
+++ libraries/base/cammWM/AssemblyVersion.vb
@@ -5,13 +5,13 @@ Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-<Assembly: AssemblyVersion("4.10.202.128")>
+<Assembly: AssemblyVersion("4.10.205.100")>
 
 Namespace CompuMaster.camm.WebManager.Setup
     ''' <summary>
     ''' Required class because code security in ASP.NET 2 environments denys the access to the file version API of windows and the GetAssemblyFileVersion method would fail with a SecurityException
     ''' </summary>
     Friend Class AssemblyVersion
-        Public Const Version As String = "4.10.202.128"
+        Public Const Version As String = "4.10.205.100"
     End Class
 End Namespace
