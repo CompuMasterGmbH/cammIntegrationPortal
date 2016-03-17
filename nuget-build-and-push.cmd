6bf8dab937b4be4518ebb7d7534aee92609777f4
Jochen Wezel <jwezel@compumaster.de>
2016-03-17 19:58:31 +0100

    fixed nuspecs + added build&pack automation scripts

--- nuget-build-and-push.cmd
+++ nuget-build-and-push.cmd
@@ -1,6 +1,25 @@
+@echo off
 ::REM -UpdateNuGetExecutable not required since it's updated by VS.NET mechanisms
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\base\cammWM\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\base\cammWM\cammWM.VS2015.OpenSource.vbproj' -verbose -NoPrompt -PushPackageToNuGetGallery"
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\adminarea\cammWM.Admin\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\adminarea\cammWM.Admin\cammWM.Admin.OpenSource.VS2015.vbproj' -verbose -NoPrompt -PushPackageToNuGetGallery"
+
+echo Build+Push cammWM / CIP.Library ?
+choice /n /c "YN" /m "(Y)es, (N)o"
+if errorlevel 2 goto FinishedBuildCIPLib
+echo Building + pushing...
+::PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\base\cammWM\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\base\cammWM\cammWM.VS2015.OpenSource.vbproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\base\cammWM\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\base\cammWM\cammWM.VS2015.OpenSource.vbproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile -PushPackageToNuGetGallery"
+:FinishedBuildCIPLib
+echo Task completed.
+echo.
+
+echo Build+Push cammWM / CIP.Admin.Library ?
+choice /n /c "YN" /m "(Y)es, (N)o"
+if errorlevel 2 goto FinishedBuildCIPAdminLib
+echo Building + pushing...
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\adminarea\cammWM.Admin\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\adminarea\cammWM.Admin\cammWM.Admin.OpenSource.VS2015.vbproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile -PushPackageToNuGetGallery"
+:FinishedBuildCIPAdminLib
+echo Task completed.
+echo.
+
 ::TODO
 ::- Mono vs. Win
 ::- ClientApp vs. WebApp vs. WebRoot-withAdminArea vs. WebRoot-withoutAdminArea
