ba882adf3d21781dd2c087797cdafa0126e02767
Jochen Wezel <jwezel@compumaster.de>
2016-09-19 12:00:54 +0200

    beta release 4.12.209.100

--- libraries/adminarea/CammWM.Admin/AssemblyVersion.vb
+++ libraries/adminarea/CammWM.Admin/AssemblyVersion.vb
@@ -3,15 +3,15 @@ Option Strict On
 
 Imports System.Reflection
 
-<Assembly: AssemblyVersion("4.10.206.108")>
-<Assembly: AssemblyInformationalVersion("4.10.206.108-beta")>
+<Assembly: AssemblyVersion("4.12.209.100")>
+<Assembly: AssemblyInformationalVersion("4.12.209.100-beta")>
 
 Namespace CompuMaster.camm.WebManager.Administration
     ''' <summary>
     ''' Required class because code security in ASP.NET 2 environments deny the access to the file version API of windows and the GetAssemblyFileVersion method would fail with a SecurityException
     ''' </summary>
     Friend Class AssemblyVersion
-        Public Const Version As String = "4.10.206.108"
+        Public Const Version As String = "4.12.209.100"
     End Class
 
 End Namespace
