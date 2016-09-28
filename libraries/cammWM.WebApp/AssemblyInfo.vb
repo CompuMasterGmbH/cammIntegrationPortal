0b9d8a0cdfe99841b904528e21c4b9616e325f13
Jochen Wezel <jwezel@compumaster.de>
2016-09-28 14:27:10 +0200

    added some web projects for continueous NuGet update testing

---
+++ libraries/cammWM.WebApp/AssemblyInfo.vb
@@ -0,0 +1,19 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Default Tag-Prefix for the designer when dragging&dropping controls into a page
+<Assembly: System.Web.UI.TagPrefix("CompuMaster.camm.WebManager.Controls", "camm")>
+
+<Assembly: AssemblyTitle("cammWM.WebApp")>
+<Assembly: AssemblyDescription("camm Integration Portal Core Library")>
+<Assembly: AssemblyCompany("CompuMaster GmbH")>
+<Assembly: AssemblyProduct("camm Integration Portal (based on camm Web-Manager)")>
+<Assembly: AssemblyCopyright("2001-2016 CompuMaster GmbH")>
+<Assembly: AssemblyTrademark("camm")>
+
+<Assembly: ComVisible(False)>
+<Assembly: Guid("875f2e64-d848-4c38-a046-bda122f669e7")>
+
+<Assembly: AssemblyVersion("4.12.209.101")>
+<Assembly: AssemblyInformationalVersion("4.12.209.101-beta")>
