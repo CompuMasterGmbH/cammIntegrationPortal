11b1603d14c712ebdd98e01953b6a95bb4b42198
Jochen Wezel <jwezel@compumaster.de>
2016-09-28 15:01:01 +0200

    added some more web projects for continueous NuGet update testing

--- nuget-build-and-push-webapp.cmd
+++ nuget-build-and-push-webapp.cmd
@@ -4,9 +4,11 @@
 echo Build+Push cammWM.WebApp Library ?
 choice /n /c "YN" /m "(Y)es, (N)o"
 if errorlevel 2 goto FinishedBuildCWMWebApp
+echo NuGet self-update...
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\cammWM.WebApp\_CreateNewNuGetPackage\DoNotModify\NuGet.exe' update -self"
 echo Building + pushing...
-::PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\smarteditor\baseeditor\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\cammWM.WebApp\cammWM.WebApp.vbproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile -PushPackageToNuGetGallery"
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\smarteditor\baseeditor\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\cammWM.WebApp\cammWM.WebApp.vbproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile 
+::PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\cammWM.WebApp\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\cammWM.WebApp\cammWM.WebApp.vbproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile -PushPackageToNuGetGallery"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\cammWM.WebApp\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\cammWM.WebApp\cammWM.WebApp.vbproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile"
 :FinishedBuildCWMWebApp
 echo Task completed.
 echo.
@@ -15,8 +17,8 @@ echo Build+Push cammWM.WebRootApp Library ?
 choice /n /c "YN" /m "(Y)es, (N)o"
 if errorlevel 2 goto FinishedBuildCWMWebRootApp
 echo Building + pushing...
-::PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\smarteditor\CommonMarkEditor\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\cammWM.WebRootApp\cammWM.WebRootApp.vbproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile -PushPackageToNuGetGallery"
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\smarteditor\CommonMarkEditor\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\cammWM.WebRootApp\cammWM.WebRootApp.vbproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile 
+::PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\cammWM.WebRootApp\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\cammWM.WebRootApp\cammWM.WebRootApp.vbproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile -PushPackageToNuGetGallery"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\cammWM.WebRootApp\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\cammWM.WebRootApp\cammWM.WebRootApp.vbproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile" 
 :FinishedBuildCWMWebRootApp
 echo Task completed.
 echo.
@@ -25,8 +27,8 @@ echo Build+Push cammWM.WebRootAdminApp Library ?
 choice /n /c "YN" /m "(Y)es, (N)o"
 if errorlevel 2 goto FinishedBuildCWMWebRootAdminApp
 echo Building + pushing...
-::PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\smarteditor\webupload\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\cammWM.WebRootAdminApp\cammWM.WebRootAdminApp.vbproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile -PushPackageToNuGetGallery"
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\smarteditor\webupload\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\cammWM.WebRootAdminApp\cammWM.WebRootAdminApp.vbproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile 
+::PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\cammWM.WebRootAdminApp\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\cammWM.WebRootAdminApp\cammWM.WebRootAdminApp.vbproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile -PushPackageToNuGetGallery"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'libraries\cammWM.WebRootAdminApp\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\libraries\cammWM.WebRootAdminApp\cammWM.WebRootAdminApp.vbproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile" 
 :FinishedBuildCWMWebRootAdminApp
 echo Task completed.
 echo.
