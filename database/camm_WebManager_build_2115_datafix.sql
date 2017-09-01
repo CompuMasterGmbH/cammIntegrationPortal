8d6e943bd42114025726c1785d0e96dd0d816461
Albert S <>
2017-09-01 09:06:22 +0200

    db patch 2115: after patching, execute the triggers
    This essentially rebuilds the tables so they'll contain the fixed data.

---
+++ database/camm_WebManager_build_2115_datafix.sql
@@ -0,0 +1,27 @@
+﻿---------------------------------------------------------------------------------------------------
+-- CLEANUP OF ALL PRE-EXISTING DATA IN PRE-STAGING-TABLES
+---------------------------------------------------------------------------------------------------
+	TRUNCATE TABLE dbo.ApplicationsRightsByUser_PreStaging4AllowDenyRules
+	TRUNCATE TABLE dbo.ApplicationsRightsByUser_PreStaging3GroupsResolved
+	TRUNCATE TABLE dbo.ApplicationsRightsByGroup_PreStaging2Inheritions
+	TRUNCATE TABLE dbo.ApplicationsRightsByUser_PreStaging2Inheritions
+	TRUNCATE TABLE dbo.ApplicationsRightsByGroup_PreStaging1ForRealServerGroup
+	TRUNCATE TABLE dbo.ApplicationsRightsByUser_PreStaging1ForRealServerGroup
+GO
+---------------------------------------------------------------------------------------------------
+-- PRE-FILL ALL PRE-STAGING-LEVELS
+---------------------------------------------------------------------------------------------------
+	-- Force first table filling (or full update on repetitions)
+	UPDATE [ApplicationsRightsByGroup]
+	SET IsDenyRule = IsDenyRule
+GO
+	-- Force first table filling (or full update on repetitions)
+	UPDATE [ApplicationsRightsByUser]
+	SET IsDenyRule = IsDenyRule
+GO
+------------------------------------------------------------------------------------------------------------
+-- Force first table filling (or full update on repetitions)
+------------------------------------------------------------------------------------------------------------
+	UPDATE [dbo].[Applications_CurrentAndInactiveOnes] 
+	SET TitleAdminArea = TitleAdminArea
+GO
