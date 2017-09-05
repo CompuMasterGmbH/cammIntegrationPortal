d7aa0c4aeb9cc0302a3d7611f482fbf186c0f5d6
Albert S <>
2017-09-05 14:21:01 +0200

    Admin library: increase assembly version

--- libraries/adminarea/CammWM.Admin/AssemblyVersion.vb
+++ libraries/adminarea/CammWM.Admin/AssemblyVersion.vb
@@ -3,15 +3,15 @@ Option Strict On
 
 Imports System.Reflection
 
-<Assembly: AssemblyVersion("4.12.2115.100")>
-<Assembly: AssemblyInformationalVersion("4.12.2115.100")>
+<Assembly: AssemblyVersion("4.12.2115.101")>
+<Assembly: AssemblyInformationalVersion("4.12.2115.101")>
 
 Namespace CompuMaster.camm.WebManager.Administration
     ''' <summary>
     ''' Required class because code security in ASP.NET 2 environments deny the access to the file version API of windows and the GetAssemblyFileVersion method would fail with a SecurityException
     ''' </summary>
     Friend Class AssemblyVersion
-        Public Const Version As String = "4.12.2115.100"
+        Public Const Version As String = "4.12.2115.101"
     End Class
 
 End Namespace
