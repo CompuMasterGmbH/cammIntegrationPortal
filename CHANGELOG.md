9ea8c0b0c27acca96cf2c3095b1be5cabef43075
Jochen Wezel <jwezel@compumaster.de>
2018-03-08 15:00:38 +0100

    fixed cleanup of deactivated user accounts with missing last login date

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,6 +18,7 @@
 
 ### Build 2117 (DB Engine)
 * :arrows_clockwise: limit retention time for log type RuntimeInformation to max 30 days (for high load reasons because of regular webcron cleanup log entries increasing table size to too large values for typical shared hostings)
+* :arrows_clockwise: fixed cleanup of deactivated user accounts with missing last login date
 
 ### Build 2115 → 2116 (ASP.NET Engine)
 * :arrows_clockwise: fixed options config page for logging not being able to save on newer .NET framework environments
--- libraries/base/cammWM/AssemblyVersion.vb
+++ libraries/base/cammWM/AssemblyVersion.vb
@@ -3,15 +3,15 @@ Option Strict On
 
 Imports System.Reflection
 
-<Assembly: AssemblyVersion("4.12.2116.102")>
-<Assembly: AssemblyInformationalVersion("4.12.2116.102")>
+<Assembly: AssemblyVersion("4.12.2117.103")>
+<Assembly: AssemblyInformationalVersion("4.12.2117.103")>
 
 Namespace CompuMaster.camm.WebManager.Setup
     ''' <summary>
     ''' Required class because code security in ASP.NET 2 environments denys the access to the file version API of windows and the GetAssemblyFileVersion method would fail with a SecurityException
     ''' </summary>
     Friend Class AssemblyVersion
-        Public Const Version As String = "4.12.2116.102"
+        Public Const Version As String = "4.12.2117.103"
     End Class
 
 End Namespace
