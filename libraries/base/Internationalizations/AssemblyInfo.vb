148e33cf5b6cb61e465b867d203cd02794d5e483
Jochen Wezel <jwezel@compumaster.de>
2019-03-14 14:09:10 +0100

    general refresh of project dependencies

--- libraries/base/Internationalizations/AssemblyInfo.vb
+++ libraries/base/Internationalizations/AssemblyInfo.vb
@@ -5,9 +5,9 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("CompuMaster")> 
 <Assembly: AssemblyProduct("")>
-<Assembly: AssemblyCopyright("2001,2002,2003,2008,2009,2015,2016 CompuMaster GmbH")>
+<Assembly: AssemblyCopyright("2001,2002,2003,2008,2009,2015,2016,2019 CompuMaster GmbH")>
 <Assembly: AssemblyTrademark("")> 
 <Assembly: CLSCompliant(True)>
 <Assembly: Guid("12D6D952-1201-47F0-A17B-430D407118B6")>
 
-<Assembly: AssemblyVersion("2016.08.16.*")>
+<Assembly: AssemblyVersion("2019.03.13.*")>
---
+++ libraries/base/Internationalizations/country-list-download/AssemblyInfo.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("CWM Country List Downloader")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("CompuMaster")>
+<Assembly: AssemblyProduct("")>
+<Assembly: AssemblyCopyright("2001,2002,2003,2008,2009,2015,2016,2019 CompuMaster GmbH")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: CLSCompliant(True)>
+<Assembly: Guid("902bf4dc-4421-418e-9d2c-3e3d4053bb9a")>
+
+<Assembly: AssemblyVersion("2019.03.13.*")>
